c89ea32d764ff599b1c6a70424ccdfd6749645d8
meuplow <gabrielgm@ufcspa.edu.br>
2021-08-20 16:52:51 -0300

    #4

--- src/main/java/com/springframework/viacep/entity/Address.java
+++ src/main/java/com/springframework/viacep/entity/Address.java
@@ -3,10 +3,53 @@
  */
 package com.springframework.viacep.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author Gabriel
  *
  */
-public class Address {
 
+@Entity
+public class Address {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String cep;
+	private String logradouro;
+
+	protected Address() {}
+
+	
+	/**
+	 * @param cep
+	 * @param logradouro
+	 */
+	public Address(String cep, String logradouro) {
+		super();
+		this.cep = cep;
+		this.logradouro = logradouro;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Endereço [id=" + id + ", cep=" + cep + ", logradouro=" + logradouro + "]";
+	}
+	
 }
