38a6bdcd14893f2048d51d9b07a756931fab8545
meuplow <gabrielgm@ufcspa.edu.br>
2021-08-26 09:15:31 -0300

    populating bd with more attributes

--- src/main/resources/hsqldb/init.sql
+++ src/main/resources/hsqldb/init.sql
@@ -2,6 +2,14 @@ DROP TABLE addresses IF EXISTS;
 
 CREATE TABLE addresses (
   id INTEGER IDENTITY PRIMARY KEY,
-  zip VARCHAR(10) NOT NULL,
-  public_place  VARCHAR(40) NOT NULL
+  zip VARCHAR(30),
+  public_place VARCHAR(30),
+  complement VARCHAR(30),
+  district VARCHAR(30),
+  city VARCHAR(30),
+  federative_unit VARCHAR(30),
+  ibge VARCHAR(30),
+  gia VARCHAR(30),
+  ddd VARCHAR(30),
+  siafi VARCHAR(30)
 );
--- src/main/resources/hsqldb/populate.sql
+++ src/main/resources/hsqldb/populate.sql
@@ -1,3 +1,6 @@
-INSERT INTO addresses (id, zip, public_place)VALUES (1, '01001000', 'Praça da Sé');
-INSERT INTO addresses (id, zip, public_place)VALUES (2, '94935410', 'Rua Lídio Batista Soares');
-INSERT INTO addresses (id, zip, public_place)VALUES (3, '90010170', 'Praça Montevidéo');
+INSERT INTO addresses (id, zip, public_place, complement, district, city, federative_unit, ibge, gia, ddd, siafi)
+	VALUES (1, '01001000', 'Praça da Sé', 'lado ímpar', 'Sé', 'São Paulo', 'SP', '3550308', '1004', '11', '7107');
+INSERT INTO addresses (id, zip, public_place, complement, district, city, federative_unit, ibge, ddd, siafi)
+	VALUES (2, '94935410', 'Rua Lídio Batista Soares', '(Vl Quitandinha)', 'COHAB', 'Cachoeirinha', 'RS', '4303103', '51', '8561');
+INSERT INTO addresses (id, zip, public_place, district, city, federative_unit, ibge, ddd, siafi)
+	VALUES (3, '90010170', 'Praça Montevidéo', 'Centro Histórico', 'Porto Alegre', 'RS', '4314902', '51', '8801');
